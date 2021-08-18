2066e94d1557cce6ae857b24aab749da92e5a955
Hardik Parwana <hardikparwana@github.com>
2021-08-18 16:11:53 -0400

    single variable working

--- examples/MatrixVariateGaussianProcess.m
+++ examples/MatrixVariateGaussianProcess.m
@@ -17,14 +17,15 @@ classdef MatrixVariateGaussianProcess <handle
         X;      % input
         Y;      % output
         N_data; % size of data
+        Ns_data % size of sampled data
         
         %
         K_obs;
         noise = 0.1;
         
         %Randomly Sampled data for hyperpeter tuning
-        X_obs;
-        Y_obs;
+        Xs;
+        Ys;
         
     end
     
@@ -53,31 +54,31 @@ classdef MatrixVariateGaussianProcess <handle
         
         end
 
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%% Methods for basic data manipulation and prediction %%%%%%%%%%%% 
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
-        function [Xs, Ys] = resample(obj,n_samples)
-            N = size(obj.X,0);
-            idx = randsample([1:1:N],min(n_sampoles,N));
-            obj.Xs = obj.X(idx,:);
-            obj.Ys = obj.Y(idx,:);            
-        end
         
         function set_XY(obj,X,Y)
+            % VERIFIED
             obj.X = X;
             obj.Y = Y;
-%             keyboard
             obj.N_data = size(X,1);
             obj.get_obs_covariance();
         end
         
         function add_data(obj,x,y)
+            % VERIFIED
             obj.X = [obj.X; x];
             obj.Y = [obj.Y; y];
             obj.N_data = size(obj.X,1);
             obj.update_obs_covariance();
+            keyboard
         end
         
         function out = evaluate_kernel(obj,x1,x2)
-            diff = norm(x1-x2,2)
+            % VERIFIED
+            diff = norm(x1-x2,2);
             out = obj.sigma^2 * exp( -diff^2 / ( 2*obj.l^2 ) );
         end
         
@@ -86,22 +87,17 @@ classdef MatrixVariateGaussianProcess <handle
             K_star = [0];
             for i=1:1:N
                 K_star(i) = obj.evaluate_kernel(obj.X(i,:),Xnew);
-%                 if i>=58
-%                     keyboard
-%                 end
             end
             out = K_star(1:N);
-%             keyboard
         end
         
         function out = get_obs_covariance(obj)
-           
+            % VERIFIED           
             N = obj.N_data;
             obj.K_obs = zeros(N,N);
             for i =1:1:N
                 for j = 1:1:N
                     cov = obj.evaluate_kernel(obj.X(i,:),obj.X(j,:));
-%                     keyboard
                     if i==j
                         obj.K_obs(i,i) = cov + obj.noise;
                     else
@@ -114,51 +110,62 @@ classdef MatrixVariateGaussianProcess <handle
             
         end
         
-        % Update comodeiance matrix given new data (run after add_data)
+        % Update covariance matrix given new data (run after add_data)
         function out = update_obs_covariance(obj)
+            % VERIFIED
             N = obj.N_data;
             x = obj.X(end,:);
             for i=1:1:N
                 cov = obj.evaluate_kernel(x, obj.X(i,:));
                 if (i == N)
-                    obj.K_obs(N, N) = cov + self.noise;
+                    obj.K_obs(N, N) = cov + obj.noise;
                 else
-                    obj.K_obs(i, N) = val;
-                    obj.K_obs(N, i) = val;
+                    obj.K_obs(i, N) = cov;
+                    obj.K_obs(N, i) = cov;
                 end
             end
             out =  obj.K_obs(1:N, 1:N);
         end
         
         function [mean, cov] = predict(obj,Xnew)
-        
+            % VERIFIED
             N = obj.N_data;
             K_inv = inv(obj.K_obs(1:N,1:N));
             k_star = obj.get_X_cov(Xnew);
-            disp(size(K_inv))
-            disp(size(k_star))
             mean = ( k_star * K_inv  ) * obj.Y(1:N,:);
             Sigma = obj.evaluate_kernel(Xnew,Xnew) + obj.noise -  k_star * K_inv * k_star';
-            cov = kron(Sigma, obj.omega);            
-            keyboard
+            cov = kron(Sigma, obj.omega);   
+        end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%% Methods for Optimizing Hyperparameters %%%%%%%%%%%% 
+        %%%% They all operate on sampled subset of training set %%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        function [Xs, Ys] = resample(obj,n_samples)
+            N = size(obj.X,1);
+            idx = randsample([1:1:N],min(n_samples,N));
+            obj.Xs = obj.X(idx,:);
+            obj.Ys = obj.Y(idx,:);      
+            obj.Ns_data = size(obj.Xs,1);
         end
         
         % Evaluate derivative of kernel (w.r.t. length scale)
         function out = dk_dl(obj,x1,x2)
             diff = norm(x1 - x2);
-            out = obj.sigma^2 * exp( -diff^2 / ( 2*obj.l^2 ) ) * ( diff^2 / (obj.j^3) );
+            out = obj.sigma^2 * exp( -diff^2 / ( 2*obj.l^2 ) ) * ( diff^2 / (obj.l^3) );
         end
         
         % Evaluate derivative of kernel (w.r.t. sigma)
         function out = dk_ds(obj,x1,x2)           
-            diff = np.linalg.norm(x1 - x2);
-            out =  2*obj.sigma * np.exp(-diff^2 / (2*obj.l^2));            
+            diff = norm(x1 - x2);
+            out =  2*obj.sigma * exp(-diff^2 / (2*obj.l^2));            
         end
         
-        % Get derivative of comodeiance matrix (w.r.t. length scale and sigma)
-        function [Kl, Ks]  = get_dK_sample(obj)
+        % Get derivative of covariance matrix (w.r.t. length scale and sigma)
+        function [Kl, Ks]  = get_dK(obj)
            
-            N = size(obj.Xs);
+            N = size(obj.Xs,1);
             Kl = zeros(N,N);
             Ks = zeros(N,N);
             for i=1:1:N
@@ -177,64 +184,113 @@ classdef MatrixVariateGaussianProcess <handle
                 end
             end     
           
+        end       
+        
+        function out = get_covariance(obj)
+            % VERIFIED           
+            N = obj.Ns_data;
+            K_obs = zeros(N,N);
+            for i =1:1:N
+                for j = 1:1:N
+                    cov = obj.evaluate_kernel(obj.Xs(i,:),obj.Xs(j,:));
+                    if i==j
+                        K_obs(i,i) = cov + 0.2*obj.noise;
+                    else
+                        K_obs(i,j) = cov;
+                        K_obs(j,i) = cov;
+                    end
+                end
+            end
+            out = K_obs(1:N,1:N);
+            
         end
         
-        
-        
+         % Compute negative log likelihood
+        function L = log_likelihood(obj)
+            n = size(obj.Xs,1); % data size
+            d = size(obj.Ys,2); % single output size
+            
+            L = -0.002;
+
+            % self.omega = np.matmul(self.L, np.transpose(self.L))
+            K = obj.get_covariance();
+
+
+            % Formula assumes prior is 0: V= omega, U=K
+            
+            A = inv(K) * obj.Ys * inv(obj.omega) * obj.Ys';               
+            L = (n*d/2)*log(2*pi) + (d/2)*log(det(K)) + (n/2)*log(det(obj.omega)) + (1/2)*trace(A);     
+              
+            iter = 1;
+            Ns = size(obj.Xs,1);
+            while (L<-0.001) && iter<20
+                obj.resample(floor(4*Ns/5))
+                K = obj.get_covariance();
+                A = inv(K) * obj.Ys * inv(obj.omega) * obj.Ys';
+                L = (n*d/2)*log(2*pi) + (d/2)*log(det(K)) + (n/2)*log(det(obj.omega)) + (1/2)*trace(A);
+                
+                iter = iter + 1;
+                
+%                 A2 = inv(obj.omega) * obj.Ys' * inv(K) * obj.Ys;
+%                 L2 = (n*d/2)*log(2*pi) + (d/2)*log(det(K)) + (n/2)*log(det(obj.omega)) + (1/2)*trace(A2);    
+            end    
+            
+            if L<-0.01
+                    disp("*************** WARN:   L < 0 **********************")
+                    L = 0.001;
+            end
+
+            obj.resample(Ns);
+
+        end
         
         % Get gradient of negative log likelihood (w.r.t. length scale, sigma, omega)
         function [dL_dl, dL_ds, dL_domega] = likelihood_gradients(obj)
-            n, d = size(obj.X);
             
-            K = obj.get_comodeiance();
-            Kl, Ks = obj.get_dK();
+            n = size(obj.Xs,1); % data size
+            d = size(obj.Ys,2); % single output size
             
-            Kinv = inv(K);
-            omegainv = inv(obj.omega);
-            A = Kinv * self.Y * omegainv * obj.Y';
-            dL_dl = (d/2)*trace(Kinv * Kl) + (1/2)*trace(-Kinv * Kl * A);
-            dL_ds = (d/2)*trace(Kinv * Ks) + (1/2)*trace( -Kinv * Ks * A );
-            dL_domega = (n/2) * omegainv' - (1/2)*(omegainv' * self.Y' * Kinv' * self.Y * omegainv' );
-        end
-
-        % Get gradient of negative log likelihood (w.r.t. length scale, sigma, omega)
-        function [dL_dl, dL_ds, dL_domega] = likelihood_gradients_sample(obj)
-            n, d = size(obj.Xs);
-            
-            K = obj.get_comodeiance_sample();
-            Kl, Ks = obj.get_dK_sample();
+            K = obj.get_covariance();
+            [Kl, Ks] = obj.get_dK();
             
             Kinv = inv(K);
             omegainv = inv(obj.omega);
-            A = Kinv * self.Y * omegainv * obj.Ys';
+            A = Kinv * obj.Ys * omegainv * obj.Ys';          
+            L = (n*d/2)*log(2*pi) + (d/2)*log(det(K)) + (n/2)*log(det(obj.omega)) + (1/2)*trace(A); 
             dL_dl = (d/2)*trace(Kinv * Kl) + (1/2)*trace(-Kinv * Kl * A);
             dL_ds = (d/2)*trace(Kinv * Ks) + (1/2)*trace( -Kinv * Ks * A );
             dL_domega = (n/2) * omegainv' - (1/2)*(omegainv' * obj.Ys' * Kinv' * obj.Ys * omegainv' );
+            
+            iter = 1;
+            Ns = size(obj.Xs,1);
+            while (L<-0.001) && iter<20
+                obj.resample(floor(4*Ns/5))
+                K = obj.get_covariance();
+                [Kl, Ks] = obj.get_dK();
+                Kinv = inv(K);
+                omegainv = inv(obj.omega);
+                A = inv(K) * obj.Ys * inv(obj.omega) * obj.Ys';
+                L = (n*d/2)*log(2*pi) + (d/2)*log(det(K)) + (n/2)*log(det(obj.omega)) + (1/2)*trace(A);
+                dL_dl = (d/2)*trace(Kinv * Kl) + (1/2)*trace(-Kinv * Kl * A);
+                dL_ds = (d/2)*trace(Kinv * Ks) + (1/2)*trace( -Kinv * Ks * A );
+                dL_domega = (n/2) * omegainv' - (1/2)*(omegainv' * obj.Ys' * Kinv' * obj.Ys * omegainv' );
+                iter = iter + 1;
+            end    
+            
+            if L<-0.01
+                    disp("*************** WARN:   L < 0 **********************")
+%                     keyboard
+                    L = 0.001;
+            end
+
+            obj.resample(Ns);
+            
+            % Note:
+            % for gradient of log |A|, see https://en.wikipedia.org/wiki/Jacobi's_formula
         end
         
-        % Compute negative log likelihood
-        function L = log_likelihood(obj)
-            n, d = size(obj.X);
-
-            % self.omega = np.matmul(self.L, np.transpose(self.L))
-            self.get_covariance()
-
-            A = inv(obj.K_obs) * obj.Y * inv(obj.omega) * self.Y';
-            L = (n*d/2)*log(2*np.pi) + (d/2)*log(det(obj.K_obs)) + (n/2)*log(det(obj.omega)) + (1/2)*trace(A);
-        end
-        
-        % Compute negative log likelihood
-        function L = log_likelihood_sample(obj)
-            n, d = size(obj.Xs);
-
-            % self.omega = np.matmul(self.L, np.transpose(self.L))
-            self.get_comodeiance()
-
-            A = inv(obj.Ks) * obj.Ys * inv(obj.omega) * self.Y';
-            L = (n*d/2)*log(2*np.pi) + (d/2)*log(det(obj.Ks)) + (n/2)*log(det(obj.omega)) + (1/2)*trace(A);
-        end
-        
-        function out = clip(x,xmax,xmin)
+      
+        function out = clip(obj,x,xmax,xmin)
             if x>xmax
                 x = xmax;
             elseif x<xmin
@@ -244,26 +300,28 @@ classdef MatrixVariateGaussianProcess <handle
         end
         
         % Optimize hyperparameters
-        function fit()
+        function fit(obj,iter_max)
             
             %initialize GP with random parameters
             params_omega = [];
             params_sigma = [];
-            parama_l = [];
+            params_l = [];
+            vals = [];
             
             iters = 0;
-            max_iters = 15000;
-            alter_iter = 30;
+            max_iters = iter_max; %15000;
+            alter_iter = 2;
             
-            grad_max = 50;
+            grad_max = 50; %50
             omega_grad_max = 40;
-            rate = 0.0005;
+            rate = 0.005;%0.0005;
             
             mode = randi(3);
             
             cur_omega = obj.omega;
             cur_sigma = obj.sigma;
             cur_l = obj.l;
+            
             
             while iters < max_iters
                 
@@ -280,50 +338,66 @@ classdef MatrixVariateGaussianProcess <handle
                 
                 % Get gradients
                 
-                
-                obj.resample(80);
-                dL_dl, dL_ds, dL_domega = gp.likelihood_gradients();
-                dL_dl = obj.clip(dL_dl, -grad_max, grad_max);
-                dL_ds = obj.clip(dL_ds, -grad_max, grad_max);
-                
-                if (max(dL_domega) > omega_grad_max || min(dL_domega) < omega_grad_max)
+                Ns_org = obj.Ns_data;
+                obj.resample(floor(4*Ns_org/2));
+%                 obj.Xs = obj.X;
+%                 obj.Ys = obj.Y;
+%                 obj.Ns_data = obj.N_data;
+                [dL_dl, dL_ds, dL_domega] = obj.likelihood_gradients();
+%                 keyboard
+                dL_dl = obj.clip(dL_dl, grad_max, -grad_max);
+                dL_ds = obj.clip(dL_ds, grad_max, -grad_max);
+                if ( (max(max(dL_domega)) > omega_grad_max) || (min(min(dL_domega)) < -omega_grad_max) )
                     
-                    max_val = max(max(dL_domega), abs(min(dL_domega)));
+                    max_val = max(max(max(dL_domega)), abs(min(min(dL_domega))));
                     dL_domega = dL_domega * (omega_grad_max / max_val);
                     
                 end
                 
                 % Gradient descent
                 eps = 0.0005;
-                
-                if mode==0
-                    cur_o = cur_o - rate * dL_domega;
-                    D, V = eig(cur_o);
-                    for i=1:1:size(D,0)
+%                 mode = 3;
+                if mode==1
+                    cur_omega = cur_omega - rate * dL_domega;
+                    try
+                        [V, D] = eig(cur_omega);
+                    catch
+                        keyboard
+                    end
+                    for i=1:1:size(D,1)
                         if (D(i) <= eps)
                             D(i) = eps;
                         end
                     end
-                    cur_o = V * diag(D) * inv(V);
-                elseif (mode == 1)
+                    cur_omega = V * D * inv(V);
+                    if det(obj.omega)<0
+                        keyboard
+                    end
+                elseif (mode == 2)
                     cur_l = cur_l - rate * dL_dl;
                     if (cur_l < eps)
                         cur_l = eps;
                     end
-                elseif (mode == 2)
-                    cur_s = cur_s - rate * dL_ds
-                    if (cur_s < eps)
-                        cur_s = eps;
+                elseif (mode == 3)
+                    cur_sigma = cur_sigma - rate * dL_ds;
+                    if (cur_sigma < eps)
+                        cur_sigma = eps;
                     end
                 else
                     disp("Error in parameter update")
+                    keyboard
+                end
+                
+                if cur_sigma<0
+                    cur_sigma = 0;
                 end
                 
                 % Update parameters
-                obj.omega = cur_o;
-                obj.sigma = cur_s;
+                obj.omega = cur_omega;
+                obj.sigma = cur_sigma;
                 obj.l = cur_l;
-                obj.omega = (obj.omega + gp.omega')/2;
+                obj.omega = (obj.omega + obj.omega')/2;
+                
                 
                 iters = iters+1; %iteration count
                 value = obj.log_likelihood();
@@ -337,22 +411,25 @@ classdef MatrixVariateGaussianProcess <handle
                     if (mode < 3)
                         mode = mode + 1;
                     elseif (mode == 3)
-                        mode = 0;
+                        mode = 1;
                     else
                         disp("Error in setting modeiable to update.")
+                        keyboard
                     end
                 end
-                
-                % Save parameters and likelihoods
+                % Save parameters and likeliobj.get_obs_covariance();obj.get_obs_covariance();hoods
                 if mod(iters, 10 == 0)
-                    fprintf("Iteration: %d", iters)
-                    fprintf("Likelihood for this dataset: %f ", value)
+                    fprintf("Iteration: %d, mode: %d, Likelihood for this dataset: %f, der: %f, %f, %f \n", iters, mode, value, dL_dl, dL_ds, dL_domega)
 %                 if (iters % 50 == 0 and kSave):
 %                     np.save('likelihood_vals_robot_v' + str(iteration), vals)
 %                     np.save('parameters_robot_v' + str(iteration), [params_omega, params_sigma, params_l]
                 end
    
             end
+            fprintf("Iteration: %d, mode: %d, Likelihood for this dataset: %f \n", iters, mode, value)
+            
+            % reset the covariance matrix
+            obj.get_obs_covariance();
         
 
         end
--- examples/test_MVG.m
+++ examples/test_MVG.m
@@ -1,33 +1,67 @@
+%% 
 clear all;
 close all;
 
-N = 100;
-X = 100*linspace(0,1,N)';
 
-X = [X,X.^2,X.^3];        
-%               keyboard
-y = 1 + X + sin(20*X*[1;-2;6]).*X.*X + 0.2*randn(N,3);  
+
+% N = 100;
+% X = 100*linspace(0,1,N)';
+% X = [X,X.^2,X.^3];        
+%y = 1 + X + sin(20*X*[1;-2;6]).*X.*X + 0.2*randn(N,3);  
+
+N = 100;
+X = 10*linspace(0,1,N)';
+X = [X];       
+y = 1 + X + X.*X + 0.2*randn(N,1);  
+
+
 % y = X.^2;
-D = size(X,2);
+% D = size(X,2);
+
+
+
+
+
+% Omega = [1 0 0;
+%          0 100 0;
+%          0 0 1000];
+% sigma = 0.2;
+% l = 10000;
+
+Omega = [1];  % between components of output vector
+sigma = 0.2;
+l = 20;
+
+
+
+gp = MatrixVariateGaussianProcess(Omega,sigma,l);
+gp.set_XY(X,y);
+
+x_new = [6.5];
+
+
+[mean, cov] = gp.predict(x_new)
+% keyboard
+
+
+% log likelihood
+gp.resample(N);
+gp.log_likelihood()
+[ll, ss, oo] = gp.likelihood_gradients()
+gp.fit(100)
+
+
+
+%Plot results
+
+
+
+figure(1)
+
+plot(X,y)
+
 
 % hold on
 % plot(y(:,1),'r')
 % plot(y(:,2),'v')
 % plot(y(:,3),'k')
-
-
-Omega = [1 0 0;
-         0 100 0;
-         0 0 10000];
-sigma = 0.2;
-l = 10000;
-
-gp = MatrixVariateGaussianProcess(Omega,sigma,l);
-gp.set_XY(X,y);
-
-x_new = [60.5, 60.5^2, 60.5^3];
-
-
-[mean, cov] = gp.predict(x_new)
-
-norm(mean - y(60,:))
