8519afc9605d1403c17e97e7fee5e1a8dfb3cc85
Hardik Parwana <hardikparwana@github.com>
2022-08-15 09:52:44 -0400

    tested tensor predictions

--- utils/test_gp.py
+++ utils/test_gp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import sys
 sys.path.append('/home/hardik/Desktop/Research/Adversary-CBF')
 from mvgp import MVGP
@@ -57,25 +58,45 @@ axis1[1].fill_between( train_x[0], ys[:,1] - factor * covs[:,1], ys[:,1] + facto
 
 gp.train(max_iters=10)
 print(f"New Parameters: omage: {gp.omega}, Sigma: {gp.sigma}, l:{gp.l}")
+gp.get_obs_covariance()
 
 # gp.omega = np.array( [ [0.9168, -0.0205],[-0.0205, 0.9809] ] )
 # gp.sigma = 0.6739
 # gp.l = 1.5527
 
-gp.get_obs_covariance()
+# ys = []
+# covs = []
+# for i in range(train_x.shape[1]):
+#     mu, cov = gp.predict( train_x[:,i].T )
+#     # print(mu,cov)
+#     if ys == []:
+#         ys = np.copy(mu)
+#         covs = np.diagonal( cov ).reshape(1,-1)
+#     else:
+#         ys = np.append( ys, mu, axis=0 )
+#         covs = np.append( covs, np.diagonal( cov ).reshape(1,-1) , axis=0)
+        
+# ## Plot
+# axis1[0].plot( train_x[0], ys[:,0], 'g', label='Trained Predicted Mean' )
+# axis1[0].fill_between( train_x[0], ys[:,0] - factor * covs[:,0], ys[:,0] + factor * covs[:,0], color="tab:blue", alpha=0.2 )
 
+# axis1[1].plot( train_x[0], ys[:,1], 'g', label='Trained Predicted Mean' )
+# axis1[1].fill_between( train_x[0], ys[:,1] - factor * covs[:,1], ys[:,1] + factor * covs[:,1], color="tab:blue", alpha=0.2 )
+
+gp.initialize_torch()
+train_x_tensor = torch.tensor(train_x)
 
 ys = []
 covs = []
-for i in range(train_x.shape[1]):
-    mu, cov = gp.predict( train_x[:,i].T )
+for i in range(train_x_tensor.shape[1]):
+    mu, cov = gp.predict_torch( train_x_tensor[:,i].T )
     # print(mu,cov)
     if ys == []:
-        ys = np.copy(mu)
-        covs = np.diagonal( cov ).reshape(1,-1)
+        ys = np.copy(mu.detach().numpy())
+        covs = np.diagonal( cov.detach().numpy() ).reshape(1,-1)
     else:
-        ys = np.append( ys, mu, axis=0 )
-        covs = np.append( covs, np.diagonal( cov ).reshape(1,-1) , axis=0)
+        ys = np.append( ys, mu.detach().numpy(), axis=0 )
+        covs = np.append( covs, np.diagonal( cov.detach().numpy() ).reshape(1,-1) , axis=0)
         
 ## Plot
 axis1[0].plot( train_x[0], ys[:,0], 'g', label='Trained Predicted Mean' )
